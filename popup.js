4bec2d69280f6f29bed9ec678f4a367e2de125cc
Kevin Röbert <dev@xn--rb-fka.it>
2017-08-05 23:22:38 +0200

    Add jquery 3.2.1 for popup

--- popup.js
+++ popup.js
@@ -1,30 +1,37 @@
 var status = "on";
-      var exception = "off";
+var exception = "off";
 
-      function changeStatus(){
-        var element = document.getElementById('status');
-        var val = "Global Enable";
-        if(status == "on"){
-          val = " Global Disable";
-          status = "off";
-          element.className = "disable";
-        }else{
-          status = "on";
-          element.className = "enable";
-        }
-        element.innerHTML = val;
-      }
+  function changeStatus(){
+    var element = $("#status");
+    var val = "Global Enable";
 
-      function handleException(){
-        var element = document.getElementById('exception');
-        var val = "Enable on page";
-        if(exception == "off"){
-          val = "Disable on page";
-          exception = "on";
-          element.className = "disable";
-        }else{
-          exception = "off";
-          element.className = "enable";
-        }
-        element.innerHTML = val;
-      }
+    if(status == "on"){
+      val = " Global Disable";
+      status = "off";
+      element.removeClass().addClass("disable");
+    }else{
+      status = "on";
+      element.removeClass().addClass("enable");
+    }
+    element.html(val);
+  };
+
+  function handleException(){
+    var element = $("#exception");
+    var val = "Enable on page";
+
+    if(exception == "off"){
+      val = "Disable on page";
+      exception = "on";
+      element.removeClass().addClass("disable");
+    }else{
+      exception = "off";
+      element.removeClass().addClass("enable");
+    }
+    element.html(val);
+  };
+
+$(document).ready(function(){
+  $("#status").on("click", changeStatus);
+  $("#exception").on("click", handleException);
+});
