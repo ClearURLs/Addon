a368967bfb2d3f24a88aa17f8834defe26184725
Kevin Röbert <dev@xn--rb-fka.it>
2018-01-16 14:42:41 +0100

    For the next version

--- core_js/write_version.js
+++ core_js/write_version.js
@@ -2,5 +2,5 @@
  * This file writes only the version into every page.
  * @return version
  */
-var version = "1.2.1.1";
+var version = browser.runtime.getManifest().version;
 $('#version').text(version);
